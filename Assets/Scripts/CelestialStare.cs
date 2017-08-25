5ab64b57a54387d11247244d84e82f336ab988c8
ZalThulu <rcopperheads11@aol.com>
2017-08-24 20:44:40 -0400

    added sun movement

---
+++ Assets/Scripts/CelestialStare.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialStare : MonoBehaviour {
+
+    public GameObject celestialCenter;
+
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(celestialCenter.transform);
+	}
+}
